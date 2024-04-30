068f9afe33814f6d19adfd7653f1ab0d9557ac95
akbor shanto <akborshanto2002@gmail.com>
2024-04-29 17:19:36 -0700

    env file

--- src/firebase/firebase.config.jsx
+++ src/firebase/firebase.config.jsx
@@ -6,14 +6,24 @@ import { getAuth } from "firebase/auth";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCVHTzGfPOl-PBLj9gvy3alVcSMfZyYzb4",
+//   authDomain: "south-asia-3915c.firebaseapp.com",
+//   projectId: "south-asia-3915c",
+//   storageBucket: "south-asia-3915c.appspot.com",
+//   messagingSenderId: "448660167874",
+//   appId: "1:448660167874:web:9b1f73377054f86d932f5b"
+
+
+// };
 const firebaseConfig = {
-  apiKey: "AIzaSyCVHTzGfPOl-PBLj9gvy3alVcSMfZyYzb4",
-  authDomain: "south-asia-3915c.firebaseapp.com",
-  projectId: "south-asia-3915c",
-  storageBucket: "south-asia-3915c.appspot.com",
-  messagingSenderId: "448660167874",
-  appId: "1:448660167874:web:9b1f73377054f86d932f5b"
-};
+  apiKey:import.meta.env.VITE_API,
+  authDomain:import.meta.env.AUTH_DOMIN,
+  projectId:import.meta.env.PROJECTID,
+  storageBucket:import.meta.env.BUCKET,
+  messagingSenderId:import.meta.env.MESSAGING_SENDERID,
+  appId:import.meta.env.APPID,
+ };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig)
