6f4f601e8da129cc8f617b479de1c786ba424009
akbor shanto <akborshanto2002@gmail.com>
2024-04-29 17:03:03 -0700

    country section

--- src/pages/home/countries-section/CountriesSection.jsx
+++ src/pages/home/countries-section/CountriesSection.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import UseTitle from "./../../../hook/useTitle/UseTitle";
+import { Link } from "react-router-dom";
 
 const CountriesSection = () => {
   const [internationSpot, setInternationalSpot] = useState([]);
@@ -9,12 +10,12 @@ const CountriesSection = () => {
       .then((res) => res.json())
       .then((data) => setInternationalSpot(data));
   }, []);
-  const handleReadMore = (id) => {
-    // Filter internationSpot to get the single item based on ID
-    const singleSpot = internationSpot.find((item) => item._id === id);
-    // Now you can do whatever you want with the singleSpot data
-    console.log(singleSpot);
-  };
+//   const handleReadMore = (id) => {
+//     // Filter internationSpot to get the single item based on ID
+//     const singleSpot = internationSpot.find((item) => item._id === id);
+//     // Now you can do whatever you want with the singleSpot data
+//     console.log(singleSpot);
+//   };
   return (
     <div>
       <UseTitle
@@ -22,36 +23,45 @@ const CountriesSection = () => {
         title="Malaysia represents fantastic value for money at almost all levels."
       ></UseTitle>
 
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-        {internationSpot?.map((data) => (
-          <div key={Math.random()}>
-            <div className="max-w-xs rounded-md shadow-md bg-gray-900 text-gray-100">
-              <img
-                src={data.img}
-                alt=""
-                className="object-cover object-center w-full rounded-t-md h-72 bg-gray-500"
-              />
-              <div className="flex flex-col justify-between p-6 space-y-8">
-                <div className="space-y-2">
-                  <h2 className="text-3xl font-semibold tracking-wide">
-                    {data.countryName}
-                  </h2>
-                  <p className="text-gray-100">
-                    {data.description.slice(0, 100)}
-                  </p>
-                </div>
-                <button
-                  onClick={() => handleReadMore(data._id)}
-                  type="button"
-                  className="flex items-center justify-center w-full p-3 font-semibold tracking-wide rounded-md bg-violet-400 text-gray-900"
-                >
-                  Read more
-                </button>
-              </div>
-            </div>
-          </div>
-        ))}
-      </div>
+	  <Link to='/allTourist'>
+
+
+
+
+
+	  
+	  <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+	  {internationSpot?.map((data) => (
+		<div key={Math.random()}>
+		  <div className="max-w-xs rounded-md shadow-md bg-gray-900 text-gray-100">
+			<img
+			  src={data.img}
+			  alt=""
+			  className="object-cover object-center w-full rounded-t-md h-72 bg-gray-500"
+			/>
+			<div className="flex flex-col justify-between p-6 space-y-8">
+			  <div className="space-y-2">
+				<h2 className="text-3xl font-semibold tracking-wide">
+				  {data.countryName}
+				</h2>
+				<p className="text-gray-100">
+				  {data.description.slice(0, 100)}
+				</p>
+			  </div>
+			 {/*  <button
+				onClick={() => handleReadMore(data._id)}
+				type="button"
+				className="flex items-center justify-center w-full p-3 font-semibold tracking-wide rounded-md bg-violet-400 text-gray-900"
+			  >
+				Read more
+			  </button> */}
+			</div>
+		  </div>
+		</div>
+	  ))}
+	</div>
+	  </Link>
+
     </div>
   );
 };
